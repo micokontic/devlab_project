de60158a637920e97d76c906dfe0f197c7a71634
Mico Kontic <micokontic@gmail.com>
2020-11-25 21:32:47 +0100

    second commit

--- apiCall.js
+++ apiCall.js
@@ -3,16 +3,21 @@ const link='https://api.spoonacular.com/recipes/complexSearch?apiKey=';
 //prva pretraga, daj mi 20 recepata za paste
 const search1='&query=pasta&number=20';
 //druga pretraga daj mi 20 recepata za paste, daj mi podatke o sastojcima, daj mi upustva za pravljenje, i recepat je potrebno da koristi tomato
-const search2='&query=pasta&number=20&fillIngredients=true&instructionsRequired=true&includeIngredients=tomato';
+const search2='&query=pasta&number=20&fillIngredients=true&instructionsRequired=true&includeIngredients=tomato,garlic';
 
 const button=document.getElementById('button');
+const naslov=document.getElementById('naslov');
+button.addEventListener('click',getData);
 
-button.addEventListener('click',getData)
 async function getData(){
-    fetch(`${link}${apiKey}${search1}`, {
+    fetch(`${link}${apiKey}${search2}`, {
     method: 'GET',
 })
     .then(response => response.json())
-    .then(json => console.log(json))
-    .catch(error => console.error(error))
+    .then((json)=>{
+        console.log(json);
+        naslov.innerHTML=(json.results[0].id);
+    })
+    .catch(error => console.error(error))   
 }
+
