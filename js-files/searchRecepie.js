fff036e7c61802cc32d41e55281d8c5c2d37d109
Mico Kontic <micokontic@gmail.com>
2020-12-02 21:06:04 +0100

    IngSearch added

--- js-files/searchRecepie.js
+++ js-files/searchRecepie.js
@@ -1,10 +1,92 @@
-
+const API_KEY10='3cd18534149a4cc1b19ebd6f1c8ef472';
+const API_KEY11='55aaab3f111448719c70a7565dd7bf38';
+const ingridientSearcButton=document.getElementById('ingridient-search');
 var ingridientsInFridge=JSON.parse(localStorage.getItem('ingridientsInFridge'));
+
+
+ingridientSearcButton.addEventListener('click',getCuisinesByIng)
 
 function createIngridientsString(){
     var ingridientsString=''
     if(ingridientsInFridge.length!=0){
         ingridientsString='&ingredients='
+        ingridientsInFridge.map((ing)=>{
+        ingridientsString=ingridientsString+ing.name+',';
+        })
+        ingridientsString=ingridientsString.slice(0, -1)
+    }else{
+        console.log('nula je');
+        ingridientsString=''
+    }
+    return ingridientsString;
+}
+
+async function getCuisinesByIng(){
+    /*var checkedValue = document.querySelector('.messageCheckbox:checked').value;*/
+    var ingString=createIngridientsString();
+    fetch(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=${API_KEY11}${ingString}&number=20`/*&type=${checkedValue}*/, {
+    method: 'GET',
+    
+})
+    .then(response => response.json())
+    .then((json)=>{
+        console.log(json);
+        console.log(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=${API_KEY11}${ingString}&number=20`);
+        displayResultCousineByIng(json)
+    })
+    .catch(error => console.error(error))   
+}
+
+function displayResultCousineByIng(json){
+    json.map((recipe)=>{
+        displayResultRecipeByIng(recipe);
+        console.log(recipe)
+    })
+
+}
+
+const displayResultRecipeByIng=(cuisine)=>{
+    const div = document.createElement('div');
+    div.innerHTML=`<img class='recepie-img'src='${cuisine.image}' alt='Ingridient Image id-${cuisine.id}'>
+    <h2>${cuisine.title}</h2>
+        <div class="ing-container">
+            <h4>USED</h4>
+            <h4 class="used white-bck"></h4>
+            <h4>MISSING</h4>
+            <h4 class="missing  white-bck"></h4>
+        </div>
+    </span>
+    `
+    var usedChild = div.querySelector('.used');
+    var missingChild=div.querySelector('.missing');
+
+    cuisine.usedIngredients.map((ing)=>{
+        let imgElem=document.createElement('img');
+        imgElem.src=ing.image;
+        usedChild.appendChild(imgElem);
+    })
+
+    cuisine.missedIngredients.map((ing)=>{
+        let imgElem=document.createElement('img');
+        imgElem.src=ing.image;
+        missingChild.appendChild(imgElem);
+    })
+    
+    /*CHECK IF MEAL IS VEGAN (ADD PSEUDO EL VEGAN IF IT IS) */
+    if(cuisine.vegan){
+        div.classList.add("greenW");
+    }else{
+        div.classList.add("redW");
+    }
+    div.classList.add("divTest");
+    cuisineResultSection.appendChild(div);
+    /* aboutCuisineTxt.innerHTML = ""; */
+}
+
+/*function createIngridientsString2(){
+    var ingridientsString=''
+    if(ingridientsInFridge.length!=0){
+        ingridientsString='&includeIngredients='
         ingridientsInFridge.map((ing)=>{
         ingridientsString=ingridientsString+ing.name+',+';
         })
@@ -15,18 +97,15 @@ function createIngridientsString(){
     }
     return ingridientsString;
 }
+*/
 
 
 
-
-async function getCuisines(cuisineId){
+/*async function getCuisines2(cuisineId){
     console.log(cuisineId);
-    /*var checkedValue = document.querySelector('.messageCheckbox:checked').value;*/
-    var ingString=createIngridientsString();
-    console.log(API_KEY2);
-    console.log(ingString);
-    
-    fetch(`https://api.spoonacular.com/recipes/findByIngredients?apiKey=${API_KEY3}${ingString}`/*&type=${checkedValue}*/, {
+    var ingString=createIngridientsString2();
+   console.log(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY}${ingString}&fillIngredients=true&number=100`)
+    fetch(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${API_KEY10}${ingString}&number=100&fillIngredients=true`, {
     method: 'GET',
     
 })
@@ -36,5 +115,7 @@ async function getCuisines(cuisineId){
     })
     .catch(error => console.error(error))   
 }
+*/
 
-getCuisines();
+//getCuisines();
+//getCuisines2();
--- js-files/shop.js
+++ js-files/shop.js
@@ -1,6 +1,9 @@
 const API_KEY='3203da816026457084c63835967a95c3';
 const API_KEY2='42c7c7a52bf844368878a3d8c96378ed';
 const API_KEY3='d22eaf828c6c4cecb77af573b2673b48';
+const API_KEY10='3cd18534149a4cc1b19ebd6f1c8ef472';
+const API_KEY11='55aaab3f111448719c70a7565dd7bf38';
+
 const LINK_INGREDIENT_SEARCH='https://api.spoonacular.com/food/ingredients/search?apiKey='
 const ingredientSearchString='&number=100&sort=calories&sortDirection=asc&query=';
 const ingSearchButton=document.getElementById('ing-search-btn');
@@ -18,7 +21,7 @@ ingSearchButton.addEventListener('click',ingredientSearch);
 
 async function ingredientSearch(){
     ingridientString=ingridientSearchInput.value;
-    ingridientResult=await fetch(`${LINK_INGREDIENT_SEARCH}${API_KEY}${ingredientSearchString}${ingridientString}&sortDirection=desc&sort=popularity`, {
+    ingridientResult=await fetch(`${LINK_INGREDIENT_SEARCH}${API_KEY11}${ingredientSearchString}${ingridientString}&sortDirection=desc&sort=popularity`, {
     method: 'GET',
 })
     .then(response => response.json())
